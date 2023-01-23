2b73ffa19d8940b950f5d8b0afca9c8cad865b52
niyati <niyatipandit08@gmail.com>
2023-01-23 21:37:55 +0530

    append new todo to table

--- app/app.js
+++ app/app.js
@@ -1,6 +1,18 @@
 function greet(){
-    var nameElm = document.getElementById('tname');
-    console.log(nameElm)
-    var elem = document.getElementById("name")
-    elem.innerHTML='<b>'+nameElm.value+'</b>'
+    var table = document.getElementById("to-do-table");
+    var todo = document.getElementById('to-do-item').value;
+
+    console.log(`adding new to-do ${todo}`);
+    
+    // 1. create row element
+    var tr = document.createElement('tr')
+    var td = document.createElement('td')
+
+    var todoTxt = document.createTextNode(todo)
+
+    td.appendChild(todoTxt)
+    tr.appendChild(td)
+    table.appendChild(tr)
+    // 2. append row element to table
+
 }
